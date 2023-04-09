f73fe0e40364eb24090c5365b4129e64b28537d1
park songhee <songhee6392@gmail.com>
2023-04-09 22:29:01 +0900

    3.5 Forms으로 React.FormEvent 추가하기, Form으로 제출하기 이벤트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+    const [value, setValue] = useState("");
+    //타입스크립트는 이 onChange 함수가 InputElement에 의해서 실행될 것을 안다
+    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        //event.currentTarget.value
+        const {
+            currentTarget: { value },
+        } = event;
+
+        setValue(event.currentTarget.value);
+    }; //React.FormEvent는 공식 문서를 가야 알 수 있음
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("제출", value);
+    };
+
     return (
         <div>
-            <Circle bgColor="teal" borderColor="black" />
-            <Circle text="텍스트 전달" bgColor="tomato" />
+            <form onSubmit={onSubmit}>
+                <input
+                    value={value}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="사용자이름"
+                />
+                <button>로그인</button>
+            </form>
         </div>
     );
 }

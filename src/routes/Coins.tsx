fdf30508388cfece062c988f00cd90a6d20cdab4
park songhee <songhee6392@gmail.com>
2023-04-11 02:10:06 +0900

    5.3 데이터 api 패칭, 로딩 스테이트 추가

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
     padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -31,54 +34,58 @@ const Coin = styled.li`
     }
 `;
 
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
 const Title = styled.h1`
     font-size: 48px;
     color: ${(props) => props.theme.accentColor};
 `;
 
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-    },
-];
+interface CoinInterface {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+}
 
 function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]); //타입을 갖은 배열이라고 알려줌
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        //즉시 실행함수
+        (async () => {
+            const response = await fetch(
+                "https://api.coinpaprika.com/v1/coins"
+            );
+            const json = await response.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
+    }, []);
+
     return (
         <Container>
             <Header>
                 <Title>코인</Title>
             </Header>
-            <CoinsList>
-                {coins.map((coin) => (
-                    <Coin key={coin.id}>
-                        <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-                    </Coin>
-                ))}
-            </CoinsList>
+            {loading ? (
+                <Loader>로딩</Loader>
+            ) : (
+                <CoinsList>
+                    {coins.map((coin) => (
+                        <Coin key={coin.id}>
+                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                        </Coin>
+                    ))}
+                </CoinsList>
+            )}
         </Container>
     );
 }

4d2398b7dabe9ca46e9d89682eb0b6787f4f2860
park songhee <songhee6392@gmail.com>
2023-04-12 02:15:03 +0900

    5.4 라우터 스테이트 추가, 아이콘 이미지 추가, 클릭했을 때 코인 이름 받아오기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,13 +1,52 @@
-import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height: 15vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    font-size: 48px;
+    color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
 
 interface RouteParams {
     coinId: string;
 }
 
+interface RounteState {
+    name: string;
+}
+
 function Coin() {
     //useParams URL에 있는 정보를 가져옴
     const { coinId } = useParams<RouteParams>();
-    return <h1>Coin : {coinId}</h1>;
+    const { state } = useLocation<RounteState>(); // dom V6 -> const name = location.state as RouterState;
+    const [loading, setLoading] = useState(true);
+
+    return (
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading"}</Title>
+            </Header>
+            {loading ? <Loader>로딩</Loader> : null}
+        </Container>
+    );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -23,9 +23,10 @@ const Coin = styled.li`
     border-radius: 15px;
     margin-bottom: 10px;
     a {
+        display: flex;
+        align-items: center;
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
     }
     &:hover {
         a {
@@ -34,14 +35,20 @@ const Coin = styled.li`
     }
 `;
 
+const Title = styled.h1`
+    font-size: 48px;
+    color: ${(props) => props.theme.accentColor};
+`;
+
 const Loader = styled.span`
     text-align: center;
     display: block;
 `;
 
-const Title = styled.h1`
-    font-size: 48px;
-    color: ${(props) => props.theme.accentColor};
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -81,7 +88,17 @@ function Coins() {
                 <CoinsList>
                     {coins.map((coin) => (
                         <Coin key={coin.id}>
-                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                            <Link
+                                to={{
+                                    pathname: `/${coin.id}`,
+                                    state: { name: coin.name },
+                                }}
+                            >
+                                <Img
+                                    src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                                />
+                                {coin.name} &rarr;
+                            </Link>
                         </Coin>
                     ))}
                 </CoinsList>
